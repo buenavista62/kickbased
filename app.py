7d0c5c50e24d612298a86dd0b533139d014ea4f9
Sercan Ates <sercan-ates@outlook.com>
2024-01-08 22:06:06 +0100

    changes

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def loadKBPlayer():
                     "TeamCover": [str(player.team_path) for player in players],
                 }
             )
-            df.to_csv("./data/kb_df.csv")
             st.session_state.players = players
     except:
         pass
